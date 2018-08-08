58499f97f8ed1af3e92ceb9c47c3ef62806050a7
MrZhangzhg <zhangzg@tedu.cn>
2018-08-08 17:28:34 +0800

    1802 py day21 17:30pm

--- nsd1802/python/day21/mysite/polls/models.py
+++ nsd1802/python/day21/mysite/polls/models.py
@@ -5,6 +5,9 @@ class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('publish date')
 
+    def __str__(self):
+        return self.question_text
+
 class Choice(models.Model):
     'django将自动在数据库表中创建一个名为id的字段作为主键'
     choice_text = models.CharField(max_length=200)
@@ -12,6 +15,9 @@ class Choice(models.Model):
     # 外键约束，django自动创建一个名为question_id的字段与polls_question
     # 表的id建立关系
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.choice_text
 # model编写完成后，需要执行以下语句
 # python manage.py makemigrations
 # python manage.py migrate
