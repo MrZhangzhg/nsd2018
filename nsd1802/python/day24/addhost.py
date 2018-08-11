c7e53386d5e59cb5fb48f0de12250c47fb1532f9
MrZhangzhg <zhangzg@tedu.cn>
2018-08-11 14:37:32 +0800

    1802 py day24 14:40am

--- nsd1802/python/day24/addhost.py
+++ nsd1802/python/day24/addhost.py
@@ -1,19 +1,19 @@
 import requests
 
 url = 'http://127.0.0.1/hosts/addhost/'
-# data = {
-#     'hostname': 'node1.tedu.cn',
-#     'ipaddr': '192.168.4.1',
-#     'group': 'webservers',
-# }
-# requests.post(url, data=data)
-hosts = [
-    {'hostname': 'node2.tedu.cn','ipaddr': '192.168.4.2','group': 'webservers',},
-    {'hostname': 'node3.tedu.cn','ipaddr': '192.168.4.3','group': 'webservers',},
-    {'hostname': 'node4.tedu.cn','ipaddr': '192.168.4.4','group': 'webservers',},
-    {'hostname': 'node5.tedu.cn','ipaddr': '192.168.4.5','group': 'dbservers',},
-    {'hostname': 'node6.tedu.cn','ipaddr': '192.168.4.6','group': 'dbservers',},
-]
-for data in hosts:
-    requests.post(url, data=data)
+data = {
+    'hostname': 'node7.tedu.cn',
+    'ipaddr': '192.168.4.7',
+    'group': 'webservers',
+}
+requests.post(url, data=data)
+# hosts = [
+#     {'hostname': 'node2.tedu.cn','ipaddr': '192.168.4.2','group': 'webservers',},
+#     {'hostname': 'node3.tedu.cn','ipaddr': '192.168.4.3','group': 'webservers',},
+#     {'hostname': 'node4.tedu.cn','ipaddr': '192.168.4.4','group': 'webservers',},
+#     {'hostname': 'node5.tedu.cn','ipaddr': '192.168.4.5','group': 'dbservers',},
+#     {'hostname': 'node6.tedu.cn','ipaddr': '192.168.4.6','group': 'dbservers',},
+# ]
+# for data in hosts:
+#     requests.post(url, data=data)
 
