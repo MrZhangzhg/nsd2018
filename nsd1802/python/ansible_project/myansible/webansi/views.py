4bca4b2c2d461717900f781ae0951b79a94da481
MrZhangzhg <zhangzg@tedu.cn>
2018-08-13 14:36:57 +0800

    1802 python ansible django day25 14:40pm

--- nsd1802/python/ansible_project/myansible/webansi/views.py
+++ nsd1802/python/ansible_project/myansible/webansi/views.py
@@ -17,8 +17,9 @@ def addhosts(request):
         ip = request.POST.get('ipaddr')
         h = request.POST.get('hostname')
         gobj = Group.objects.get_or_create(group=g)[0]
-        host = Host(hostname=h, ipaddr=ip, group=gobj)
-        host.save()
+        # host = Host(hostname=h, ipaddr=ip, group=gobj)
+        # host.save()
+        Host.objects.get_or_create(hostname=h, ipaddr=ip, group=gobj)
 
     host_info = {}
     # {'webservers': ['node1', 'node2'], 'dbservers': 'hosts'}
@@ -32,7 +33,23 @@ def addhosts(request):
     return render(request, 'webansi/addhosts.html', {'host_info': host_info})
 
 def addmodules(request):
-    return render(request, 'webansi/addmodules.html')
+    if request.method == 'POST':
+        m = request.POST.get('module')
+        a = request.POST.get('args')
+        mobj = Module.objects.get_or_create(mod_name=m)[0]
+        # args = Args(mod_args=a, mod=mobj)
+        # args.save()
+        Args.objects.get_or_create(mod_args=a, mod=mobj)
+
+    mod_info = {}
+    mods = Module.objects.all()
+    for m in mods:
+        argss = []
+        for args in m.args_set.all():
+            argss.append(args.mod_args)
+        mod_info[m] = argss
+
+    return render(request, 'webansi/addmodules.html', {'mod_info': mod_info})
 
 def tasks(request):
     return HttpResponse('tasks')
