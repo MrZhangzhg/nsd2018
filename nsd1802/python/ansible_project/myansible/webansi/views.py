78dcfbccf645129994afb66d26c1ae6f0c2194e4
MrZhangzhg <zhangzg@tedu.cn>
2018-08-13 16:17:49 +0800

    1802 python ansible django day25 finish

--- nsd1802/python/ansible_project/myansible/webansi/views.py
+++ nsd1802/python/ansible_project/myansible/webansi/views.py
@@ -1,6 +1,13 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
+import shutil
+from collections import namedtuple
+from ansible.parsing.dataloader import DataLoader
+from ansible.vars.manager import VariableManager
+from ansible.inventory.manager import InventoryManager
+from ansible.playbook.play import Play
+from ansible.executor.task_queue_manager import TaskQueueManager
+import ansible.constants as C
 from django.shortcuts import render, HttpResponse
 from .models import Host, Group, Module, Args
 
@@ -51,5 +58,66 @@ def addmodules(request):
 
     return render(request, 'webansi/addmodules.html', {'mod_info': mod_info})
 
+def exec_task(dest, mod, args):
+    Options = namedtuple('Options',
+         ['connection', 'module_path', 'forks', 'become', 'become_method', 'become_user', 'check', 'diff'])
+    options = Options(connection='smart', module_path=['/to/mymodules'], forks=10, become=None, become_method=None,
+                      become_user=None, check=False, diff=False)
+
+    loader = DataLoader()
+    passwords = dict()
+
+    inventory = InventoryManager(loader=loader, sources=['ansicfg/dhosts.py'])
+    variable_manager = VariableManager(loader=loader, inventory=inventory)
+    play_source = dict(
+        name="Ansible Play",
+        hosts=dest,
+        gather_facts='no',
+        tasks=[
+            dict(action=dict(module=mod, args=args), register='shell_out'),
+        ]
+    )
+
+    play = Play().load(play_source, variable_manager=variable_manager, loader=loader)
+
+    tqm = None
+    try:
+        tqm = TaskQueueManager(
+            inventory=inventory,
+            variable_manager=variable_manager,
+            loader=loader,
+            options=options,
+            passwords=passwords,
+        )
+        result = tqm.run(play)
+    finally:
+        if tqm is not None:
+            tqm.cleanup()
+
+        shutil.rmtree(C.DEFAULT_LOCAL_TMP, True)
+
 def tasks(request):
-    return render(request, 'webansi/tasks.html')
+    if request.method == 'POST':
+        ip = request.POST.get('ipaddr')
+        group = request.POST.get('group')
+        mod = request.POST.get('module')
+        args = request.POST.get('args')
+        print ip, group, mod, args
+        if ip:
+            dest = ip
+        else:
+            dest = group
+        exec_task(dest, mod, args)
+
+    hosts = list(Host.objects.all())
+    groups = list(Group.objects.all())
+    mod_info = {}
+    mods = Module.objects.all()
+    for m in mods:
+        argss = []
+        for args in m.args_set.all():
+            argss.append(args.mod_args)
+        mod_info[m] = argss
+    result = {'hosts': hosts, 'groups': groups, 'mods': mods, 'args': args, 'mod_info': mod_info}
+
+    return render(request, 'webansi/tasks.html', result)
