1744fb2955456102e2ecfba8fdd909bdf2f06341
MrZhangzhg <zhangzg@tedu.cn>
2018-08-27 16:03:23 +0800

    1803 py day11 16pm

--- nsd1803/python/day11/dbconn.py
+++ nsd1803/python/day11/dbconn.py
@@ -2,12 +2,13 @@
 
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, Date, ForeignKey
 from sqlalchemy.orm import sessionmaker
 
 engine = create_engine(
-    'mysql+pymysql://root:tedu.cn@localhost/tarena',
-    encoding='utf8', echo=True
+    'mysql+pymysql://root:tedu.cn@localhost/tarena?charset=utf8',
+    encoding='utf8'
+    # echo=True
 )
 # mysql+pymysql://用户名:密码@主机/库名
 Base = declarative_base()  # 创建ORM所需要的基类
@@ -18,6 +19,30 @@ class Departments(Base):
     dep_id = Column(Integer, primary_key=True)  # dep_id是表中字段
     dep_name = Column(String(20), unique=True)  # dep_name是字段
 
+    def __str__(self):
+        return '[%s: %s]' % (self.dep_id, self.dep_name)
+
+class Employees(Base):
+    __tablename__ = 'employees'
+
+    emp_id = Column(Integer, primary_key=True)
+    emp_name = Column(String(20), nullable=False)
+    gender = Column(String(6))
+    birth_date = Column(Date)
+    email = Column(String(50))
+    dep_id = Column(Integer, ForeignKey('departments.dep_id'))
+
+    def __str__(self):
+        return '[%s: %s]' % (self.emp_id, self.emp_name)
+
+class Salary(Base):
+    __tablename__ = 'salary'
+
+    auto_id = Column(Integer, primary_key=True)
+    date = Column(Date)
+    emp_id = Column(Integer, ForeignKey('employees.emp_id'))
+    basic = Column(Integer)
+    awards = Column(Integer)
+
 if __name__ == '__main__':
     Base.metadata.create_all(engine)  # 没有时创建，已有不创建
-
--- nsd1803/python/day11/insert_tarena1.py
+++ nsd1803/python/day11/insert_tarena1.py
@@ -1,11 +1,28 @@
-from dbconn import Session, Departments
+from dbconn import Session, Departments, Employees
 
-hr = Departments(dep_name='hr')
-print(hr.dep_id)    # 此时还没有在数据库中创建记录，所以是None
-print(hr.dep_name)
+# hr = Departments(dep_name='hr')
+# print(hr.dep_id)    # 此时还没有在数据库中创建记录，所以是None
+# print(hr.dep_name)
+#
+# session = Session()  # 建立到数据库的会话连接
+# session.add(hr)      # 真正向数据库写入记录
 
-session = Session()  # 建立到数据库的会话连接
-session.add(hr)      # 真正向数据库写入记录
+# op = Departments(dep_id=2, dep_name='运维部')
+# session = Session()
+# session.add(op)
+
+# dev = Departments(dep_id=3, dep_name='开发部')
+# qa = Departments(dep_id=4, dep_name='测试部')
+# session = Session()
+# session.add_all([dev, qa])
+
+bob = Employees(emp_id=1, emp_name='Bob', gender='male', birth_date='1995-08-23', email='bob@tedu.cn', dep_id='1')
+john = Employees(emp_id=2, emp_name='John', gender='male', birth_date='1992-06-2', email='john@tedu.cn', dep_id='1')
+alice = Employees(emp_id=3, emp_name='Alice', gender='female', birth_date='1997-02-2', email='alice@tedu.cn', dep_id='2')
+jane = Employees(emp_id=4, emp_name='Jane', gender='female', birth_date='1988-09-23', email='jane@tedu.cn', dep_id='2')
+tom = Employees(emp_id=5, emp_name='Tom', gender='male', birth_date='1994-04-6', email='tom@tedu.cn', dep_id='3')
+session = Session()
+session.add_all([bob, john, alice, jane, tom])
+
 session.commit()
-print(hr.dep_id)
 session.close()
