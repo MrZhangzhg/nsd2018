e92df8a0651b6bed70dbfa5c2157bfee603a1753
MrZhangzhg <zhangzg@tedu.cn>
2018-09-07 14:54:27 +0800

    1803 py day20 15pm

--- nsd1803/python/day20/mysite/polls/views.py
+++ nsd1803/python/day20/mysite/polls/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, HttpResponse
 
 def polls_index(request):
-    return HttpResponse('<h1>Polls index</h1>')
+    # return HttpResponse('<h1>Polls index</h1>')
+    return render(request, 'polls/polls_index.html')
