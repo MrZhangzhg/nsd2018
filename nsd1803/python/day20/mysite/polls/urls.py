3941f2d3953878cc6838273137c25f8e85e48ff8
MrZhangzhg <zhangzg@tedu.cn>
2018-09-10 14:38:09 +0800

    1803 py day21 15pm

--- nsd1803/python/day20/mysite/polls/urls.py
+++ nsd1803/python/day20/mysite/polls/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.polls_index, name='polls_index'),
+    url(r'^(?P<q_id>\d+)/$', views.detail, name='detail')
 ]
--- nsd1803/python/day20/mysite/polls/views.py
+++ nsd1803/python/day20/mysite/polls/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render, HttpResponse
 def polls_index(request):
     # return HttpResponse('<h1>Polls index</h1>')
     return render(request, 'polls/polls_index.html')
+
+def detail(request, q_id):
+    # return HttpResponse('你正在查看第%s个问题' % q_id)
+    return render(request, 'polls/detail.html', {'question_id': q_id})
