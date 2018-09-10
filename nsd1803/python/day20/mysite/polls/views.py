ffd3d4c58b6fcabeb330ecf7d3dc0a6d4b41d9a3
MrZhangzhg <zhangzg@tedu.cn>
2018-09-10 17:28:51 +0800

    1803 py day21 17：30pm

--- nsd1803/python/day20/mysite/polls/views.py
+++ nsd1803/python/day20/mysite/polls/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, HttpResponse
+from django.shortcuts import get_object_or_404, redirect
 from .models import Question
 
 def polls_index(request):
@@ -11,10 +12,17 @@ def polls_index(request):
 
 def detail(request, q_id):
     # return HttpResponse('你正在查看第%s个问题' % q_id)
-    return render(request, 'polls/detail.html', {'question_id': q_id})
+    # question = Question.objects.get(pk=q_id)
+    question = get_object_or_404(Question, pk=q_id)
+    return render(request, 'polls/detail.html', {'question': question})
 
 def result(request, q_id):
     return render(request, 'polls/result.html', {'question_id': q_id})
 
 def vote(request, q_id):
-    pass
+    question = get_object_or_404(Question, pk=q_id)
+    c_id = request.POST.get('c_id')
+    choice = question.choice_set.get(pk=c_id)
+    choice.votes += 1
+    choice.save()
+    return redirect('result', q_id=q_id)  # redirect不携带参数，访问新网页
