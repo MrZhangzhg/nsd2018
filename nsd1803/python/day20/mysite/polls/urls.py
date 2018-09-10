e43f9180d01f9ac1e21a8782972eb1942b0a8889
MrZhangzhg <zhangzg@tedu.cn>
2018-09-10 15:46:49 +0800

    1803 py day21 16pm

--- nsd1803/python/day20/mysite/polls/urls.py
+++ nsd1803/python/day20/mysite/polls/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.polls_index, name='polls_index'),
-    url(r'^(?P<q_id>\d+)/$', views.detail, name='detail')
+    url(r'^(?P<q_id>\d+)/$', views.detail, name='detail'),
+    url(r'^(?P<q_id>\d+)/result/$', views.result, name='result'),
+    url(r'^(?P<q_id>\d+)/vote/$', views.vote, name='vote'),
 ]
--- nsd1803/python/day20/mysite/polls/views.py
+++ nsd1803/python/day20/mysite/polls/views.py
@@ -1,9 +1,20 @@
 from django.shortcuts import render, HttpResponse
+from .models import Question
 
 def polls_index(request):
     # return HttpResponse('<h1>Polls index</h1>')
-    return render(request, 'polls/polls_index.html')
+    # return render(request, 'polls/polls_index.html')
+    # 按发布日期降序排列，取最新的5个问题
+    latest_qlist = Question.objects.order_by('-publish_date')[:5]
+    context = {'qlist': latest_qlist}
+    return render(request, 'polls/polls_index.html', context)
 
 def detail(request, q_id):
     # return HttpResponse('你正在查看第%s个问题' % q_id)
     return render(request, 'polls/detail.html', {'question_id': q_id})
+
+def result(request, q_id):
+    return render(request, 'polls/result.html', {'question_id': q_id})
+
+def vote(request, q_id):
+    pass
