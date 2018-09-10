9cde70148bb060c40176c39efad4f349558f43fb
MrZhangzhg <zhangzg@tedu.cn>
2018-09-10 12:02:43 +0800

    1803 py day21 12am

--- nsd1803/python/day20/mysite/polls/models.py
+++ nsd1803/python/day20/mysite/polls/models.py
@@ -1,8 +1,14 @@
+from datetime import timedelta
+from django.utils import timezone
 from django.db import models
+
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     publish_date = models.DateTimeField()
+
+    def was_published_recently(self):
+        return self.publish_date >= timezone.now() - timedelta(days=1)
 
     def __str__(self):
         return self.question_text
@@ -12,3 +18,5 @@ class Choice(models.Model):
     votes = models.IntegerField(default=0)
     question = models.ForeignKey(Question, models.CASCADE)
 
+    def __str__(self):
+        return "%s-> %s" % (self.choice_text, self.votes)
