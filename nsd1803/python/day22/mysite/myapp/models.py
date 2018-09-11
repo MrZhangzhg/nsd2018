5589f1928dab9c788398e0384930b3094454f66e
MrZhangzhg <zhangzg@tedu.cn>
2018-09-11 18:12:05 +0800

    1803 py day22 18pm

--- nsd1803/python/day22/mysite/myapp/models.py
+++ nsd1803/python/day22/mysite/myapp/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Department(models.Model):
+    dep_name = models.CharField(max_length=50, unique=True)
+    def __str__(self):
+        return self.dep_name
+
+class Employee(models.Model):
+    emp_name = models.CharField(max_length=20)
+    phone = models.CharField(max_length=11)
+    depart = models.ForeignKey(Department, models.CASCADE)
+    def __str__(self):
+        return self.emp_name
