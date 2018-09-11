9c715d9d9f64ac5615e69eda06501912bb2245b4
MrZhangzhg <zhangzg@tedu.cn>
2018-09-11 11:52:03 +0800

    1803 py day22 12am

--- nsd1803/python/day22/mysite/myapp/urls.py
+++ nsd1803/python/day22/mysite/myapp/urls.py
@@ -2,5 +2,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+    url(r'^$', views.index, name='index'),
     url(r'^hello/$', views.hello, {'age': 23}, name='hello'),
+    url(r'^(?P<name>[a-zA-Z]+)/(?P<age>\d+)/', views.info, name='info'),
 ]
--- nsd1803/python/day22/mysite/myapp/views.py
+++ nsd1803/python/day22/mysite/myapp/views.py
@@ -2,3 +2,13 @@ from django.shortcuts import render, HttpResponse
 
 def hello(request, age):
     return HttpResponse('Hello World! %s' % age)
+
+def info(request, name, age):
+    return HttpResponse('%s is %s years old.' % (name, age))
+
+def index(request):
+    if request.method == 'POST':
+        user = request.POST.get('xm')
+    else:
+        user = None
+    return render(request, 'index.html', {'user': user})
