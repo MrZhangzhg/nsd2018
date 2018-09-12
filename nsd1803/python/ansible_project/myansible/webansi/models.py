3c92c378e15e56979267cce55dee5e972c1ba9aa
MrZhangzhg <zhangzg@tedu.cn>
2018-09-12 11:39:58 +0800

    1803 py day23 12m

--- nsd1803/python/ansible_project/myansible/webansi/models.py
+++ nsd1803/python/ansible_project/myansible/webansi/models.py
@@ -3,4 +3,29 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class Group(models.Model):
+    hostgroup = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.hostgroup
+
+class Host(models.Model):
+    hostname = models.CharField(max_length=50)
+    ipaddr = models.CharField(max_length=15)
+    group = models.ForeignKey(Group, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return '<%s: %s>' % (self.hostname, self.ipaddr)
+
+class Module(models.Model):
+    module_name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.module_name
+
+class Args(models.Model):
+    args_text = models.CharField(max_length=100)
+    module = models.ForeignKey(Module, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.args_text
--- nsd1803/python/ansible_project/myansible/webansi/views.py
+++ nsd1803/python/ansible_project/myansible/webansi/views.py
@@ -1,8 +1,18 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
 from django.shortcuts import render, HttpResponse
 
 def mainpage(request):
     return HttpResponse('OK')
 
+def index(request):
+    return HttpResponse('web ansi index')
+
+def addhosts(request):
+    return HttpResponse('add hosts')
+
+def addmodules(request):
+    return HttpResponse('add modules')
+
+def tasks(request):
+    return HttpResponse('execute task')
