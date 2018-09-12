c076236402db61ebbf080722720e1e1697e90825
MrZhangzhg <zhangzg@tedu.cn>
2018-09-12 17:07:53 +0800

    1803 py day23 17pm

--- nsd1803/python/ansible_project/myansible/webansi/views.py
+++ nsd1803/python/ansible_project/myansible/webansi/views.py
@@ -3,7 +3,7 @@ from __future__ import unicode_literals
 from django.shortcuts import render, HttpResponse
 
 def mainpage(request):
-    return HttpResponse('OK')
+    return render(request, 'webansi/mainpage.html')
 
 def index(request):
     return HttpResponse('web ansi index')
