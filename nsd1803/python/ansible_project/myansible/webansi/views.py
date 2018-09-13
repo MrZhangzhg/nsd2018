e3afbd54b4fff34d2d503e3ed277522422745e78
MrZhangzhg <zhangzg@tedu.cn>
2018-09-13 14:13:30 +0800

    1803 py day24 14:20pm

--- nsd1803/python/ansible_project/myansible/webansi/views.py
+++ nsd1803/python/ansible_project/myansible/webansi/views.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from django.shortcuts import render, HttpResponse
-from .models import Group
+from .models import Group, Module
 
 def mainpage(request):
     return render(request, 'webansi/mainpage.html')
@@ -15,14 +15,20 @@ def addhosts(request):
         host = request.POST.get('host')
         ip = request.POST.get('ip')
         hostgroup = Group.objects.get_or_create(hostgroup=group)[0]
-        print(hostgroup)
         hostgroup.host_set.get_or_create(hostname=host, ipaddr=ip)
 
     groups = Group.objects.all()
     return render(request, 'webansi/addhosts.html', {'groups': groups})
 
 def addmodules(request):
-    return HttpResponse('add modules')
+    if request.method == 'POST':
+        mod = request.POST.get('mod')
+        param = request.POST.get('param')
+        module = Module.objects.get_or_create(module_name=mod)[0]
+        module.args_set.get_or_create(args_text=param)
+
+    modules = Module.objects.all()
+    return render(request, 'webansi/addmodules.html', {'modules': modules})
 
 def tasks(request):
     return HttpResponse('execute task')
