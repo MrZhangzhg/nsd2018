176e36a183814ee509006092881390e548de6df0
MrZhangzhg <zhangzg@tedu.cn>
2018-09-26 10:29:00 +0800

    1804 py day08 10:30am

--- nsd1804/python/day08/backup.py
+++ nsd1804/python/day08/backup.py
@@ -6,12 +6,14 @@ import pickle as p
 
 def check_md5(fname):
     m = hashlib.md5()
+
     with open(fname, 'rb') as fobj:
         while True:
             data = fobj.read(4096)
             if not data:
                 break
             m.update(data)
+
     return m.hexdigest()
 
 def full_backup(src_dir, dst_dir, md5file):
@@ -33,7 +35,28 @@ def full_backup(src_dir, dst_dir, md5file):
         p.dump(md5dict, fobj)
 
 def incr_backup(src_dir, dst_dir, md5file):
-    pass
+    fname = '%s_incr_%s.tar.gz' % (
+        os.path.basename(src_dir.rstrip('/')), time.strftime('%Y%m%d')
+    )
+    fname = os.path.join(dst_dir, fname)
+    new_md5 = {}
+
+    with open(md5file, 'rb') as fobj:
+        old_md5 = p.load(fobj)
+
+    for path, folders, files in os.walk(src_dir):
+        for each_file in files:
+            key = os.path.join(path, each_file)
+            new_md5[key] = check_md5(key)
+
+    with open(md5file, 'wb') as fobj:
+        p.dump(new_md5, fobj)
+
+    tar = tarfile.open(fname, 'w:gz')
+    for key in new_md5:
+        if old_md5.get(key) != new_md5[key]:
+            tar.add(key)
+    tar.close()
 
 if __name__ == '__main__':
     src_dir = '/tmp/security'
@@ -41,7 +64,7 @@ if __name__ == '__main__':
     md5file = '/tmp/backup/md5.data'
     if not os.path.exists(dst_dir):
         os.mkdir(dst_dir)
-    if time.strftime('%a') == 'Wed':
+    if time.strftime('%a') == 'Mon':
         full_backup(src_dir, dst_dir, md5file)
     else:
         incr_backup(src_dir, dst_dir, md5file)
