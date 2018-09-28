3c120e9d167e4eacb15c103ec6715c785c822f83
MrZhangzhg <zhangzg@tedu.cn>
2018-09-28 16:00:01 +0800

    1804 py day09 16pm

---
+++ nsd1804/python/day09/mtadd.py
@@ -0,0 +1,20 @@
+import time
+import threading
+
+def add():
+    result = 0
+    for i in range(1, 50000001):
+        result += i
+    print(result)
+
+if __name__ == '__main__':
+    start = time.time()
+    tlist = []
+    for i in range(2):
+        t = threading.Thread(target=add)
+        tlist.append(t)  # 将线程加入到列表
+        t.start()
+    for t in tlist:
+        t.join()  # 与waitpid类似，等待工作线程结束之后才会继续向下执行
+    end = time.time()
+    print(end - start)
