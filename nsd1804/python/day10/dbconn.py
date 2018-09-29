ae7b0a5c44defcc69f79ea0ed6173e4e086ace2e
MrZhangzhg <zhangzg@tedu.cn>
2018-09-29 15:42:49 +0800

    1804 py day10 15:40pm

--- nsd1804/python/day10/dbconn.py
+++ nsd1804/python/day10/dbconn.py
@@ -1,9 +1,9 @@
-from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy import create_engine, Column, Integer, String, Date, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 engine = create_engine(
-    'mysql+pymysql://root:tedu.cn@127.0.0.1/tedu1804',
+    'mysql+pymysql://root:tedu.cn@127.0.0.1/tedu1804?charset=utf8',
     encoding='utf8',
     echo=True  # 屏幕输出日志信息
 )
@@ -14,6 +14,22 @@ class Department(Base):
     __tablename__ = 'department'   # 对应到数据库中的表名
     dep_id = Column(Integer, primary_key=True)
     dep_name = Column(String(20), unique=True, nullable=False)
+
+    def __str__(self):
+        return '<部门: %s>' % self.dep_name
+
+class Employee(Base):
+    __tablename__ = 'employee'
+    emp_id = Column(Integer, primary_key=True)
+    emp_name = Column(String(20), nullable=False)
+    gender = Column(String(6))
+    birth_date = Column(Date)
+    phone = Column(String(11), unique=True)
+    email = Column(String(50), unique=True)
+    dep_id = Column(Integer, ForeignKey('department.dep_id'))
+
+    def __str__(self):
+        return '<员工: %s>' % self.emp_name
 
 if __name__ == '__main__':
     # 在数据库中创建表，如果表已存在则忽略
--- nsd1804/python/day10/insert_sqlalchemy1.py
+++ nsd1804/python/day10/insert_sqlalchemy1.py
@@ -1,9 +1,40 @@
-from dbconn import Department, Session
+from dbconn import Department, Employee, Session
 
 session = Session()
 dep_dev = Department(dep_name='development')
+dep_hr = Department(dep_name='人事部')
+dep_op = Department(dep_name='运维部')
+dep_fn = Department(dep_id=2, dep_name='财务部')
 # print(dep_dev.dep_name)
+# print(dep_dev)
 # print(dep_dev.dep_id)
-session.add(dep_dev)
+# session.add(dep_dev)
+# session.add_all([dep_hr, dep_op])
+# session.add(dep_fn)
+wh = Employee(
+    emp_name='王贺',
+    gender='male',
+    birth_date='1993-1-1',
+    phone='17788990022',
+    email='wh@163.com',
+    dep_id=1
+)
+lj = Employee(
+    emp_name='李俊',
+    gender='male',
+    birth_date='1995-10-1',
+    phone='13355667788',
+    email='lijun@163.com',
+    dep_id=4
+)
+zzh = Employee(
+    emp_name='赵子浩',
+    gender='male',
+    birth_date='1999-5-4',
+    phone='13556789987',
+    email='zzh@qq.com',
+    dep_id=4
+)
+session.add_all([wh, lj, zzh])
 session.commit()
 session.close()
