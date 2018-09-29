75b65470d5baa9513448f0fb3bf904956960219b
MrZhangzhg <zhangzg@tedu.cn>
2018-09-29 17:37:12 +0800

    1804 py day10 17:40pm

--- nsd1804/python/day10/query_sqlalchemy1.py
+++ nsd1804/python/day10/query_sqlalchemy1.py
@@ -35,12 +35,65 @@ for dep in query5:
     print(dep)
 print('#' * 40)
 
-query6 = session.query(Department).filter(Department.dep_id==2)
+query6 = session.query(Department).filter(Department.dep_id == 2)
 print(query6)  # 返回SQL语句
 for dep in query6:  # 即使过滤的结果只有一项，也会放到列表中
     print(dep)
+print('#' * 40)
 
+query7 = session.query(Department).filter(Department.dep_id>2)\
+    .filter(Department.dep_id<4)  # 多次filter就是where...and...
+print(query7)
+for dep in query7:
+    print(dep)
+print('#' * 40)
 
+query8 = session.query(Department).filter(Department.dep_id!=2)
+print(query8)  # 返回SQL语句
+for dep in query8:  # 即使过滤的结果只有一项，也会放到列表中
+    print(dep)
+print('#' * 40)
 
+query9 = session.query(Department).filter(Department.dep_name.like('%部'))
+print(query9)    # like与mysql查询一样，%表示0到多个字符
+for dep in query9:
+    print(dep)
+print('#' * 40)
 
+query10 = session.query(Department).filter(Department.dep_id.in_([1,3,4]))
+print(query10)
+for dep in query10:
+    print(dep)
+print('#' * 40)
 
+query11 = session.query(Department).filter(~Department.dep_id.in_([1,3,4]))
+print(query11)  # ~表示取反
+for dep in query11:
+    print(dep)
+print('#' * 40)
+
+query12 = session.query(Department).filter(Department.dep_name.is_(None))
+print(query12)
+for dep in query12:
+    print(dep)
+print('#' * 40)
+
+query13 = session.query(Department).filter(Department.dep_name.isnot(None))
+print(query13)
+for dep in query13:
+    print(dep)
+print('#' * 40)
+
+from sqlalchemy import and_, or_
+query14 = session.query(Department)\
+    .filter(and_(Department.dep_id>2, Department.dep_id<4))
+print(query14)
+for dep in query14:
+    print(dep)
+print('#' * 40)
+
+query15 = session.query(Department)\
+    .filter(or_(Department.dep_id<2, Department.dep_id>3))
+print(query15)
+for dep in query15:
+    print(dep)
