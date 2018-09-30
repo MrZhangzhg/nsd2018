8e3605708926faf0cf07290e08d19767094955fc
MrZhangzhg <zhangzg@tedu.cn>
2018-09-30 14:53:28 +0800

    1804 py day11 15pm

--- nsd1804/python/day11/rcmd.py
+++ nsd1804/python/day11/rcmd.py
@@ -1,4 +1,8 @@
 import paramiko
+import sys
+import getpass
+import os
+from threading import Thread
 
 def rcmd(host, passwd, command, user='root', port=22):
     ssh = paramiko.SSHClient()
@@ -11,6 +15,22 @@ def rcmd(host, passwd, command, user='root', port=22):
         print('%s[OUT]:\n%s' % (host, out.decode()))
     if err:
         print('%s[ERROR]:\n%s' % (host, err.decode()))
+    ssh.close()
 
 if __name__ == '__main__':
-    rcmd('192.168.4.2', '123456', 'id zhangsan; id wangwu')
+    if len(sys.argv) != 3:
+        print('Usage: %s ipfile "command"' % sys.argv[0])
+        sys.exit(1)
+    ipfile = sys.argv[1]
+    command = sys.argv[2]
+    passwd = getpass.getpass()
+    if not os.path.isfile(ipfile):
+        print('No such file:', ipfile)
+        sys.exit(2)
+    with open(ipfile) as fobj:
+        for line in fobj:
+            ip = line.strip()
+            # rcmd(ip, passwd, command)
+            t = Thread(target=rcmd, args=(ip, passwd, command))
+            t.start()
+# python3 rcmd.py ipaddr.txt "sleep 5;id zhangsan"
