a1545504460418e867e8864b7d05cc8fee5d7d68
MrZhangzhg <zhangzg@tedu.cn>
2018-10-10 15:40:10 +0800

    1804 py day14 15:40pm

---
+++ nsd1804/python/day14/exec_task.py
@@ -0,0 +1,58 @@
+import shutil
+from collections import namedtuple
+from ansible.parsing.dataloader import DataLoader
+from ansible.vars.manager import VariableManager
+from ansible.inventory.manager import InventoryManager
+from ansible.playbook.play import Play
+from ansible.executor.task_queue_manager import TaskQueueManager
+import ansible.constants as C
+
+# since API is constructed for CLI it expects certain options to always be set, named tuple 'fakes' the args parsing options object
+Options = namedtuple('Options', ['connection', 'module_path', 'forks', 'become', 'become_method', 'become_user', 'check', 'diff'])
+options = Options(connection='smart', module_path=['/to/mymodules'], forks=10, become=None, become_method=None, become_user=None, check=False, diff=False)
+
+# initialize needed objects
+loader = DataLoader() # Takes care of finding and reading yaml, json and ini files
+passwords = dict()
+
+# create inventory, use path to host config file as source or hosts in a comma separated string
+# inventory = InventoryManager(loader=loader, sources='localhost,')
+inventory = InventoryManager(loader=loader, sources=['myansi/hosts'])
+
+# variable manager takes care of merging all the different sources to give you a unifed view of variables available in each context
+variable_manager = VariableManager(loader=loader, inventory=inventory)
+
+# create datastructure that represents our play, including tasks, this is basically what our YAML loader does internally.
+play_source =  dict(
+        name = "Ansible Play",
+        hosts = 'dbservers',
+        gather_facts = 'no',
+        tasks = [
+            dict(action=dict(module='yum', args='name=vsftpd state=present'), register='shell_out'),
+            # dict(action=dict(module='shell', args='id zhangsan'), register='shell_out'),
+            # dict(action=dict(module='debug', args=dict(msg='{{shell_out.stdout}}')))
+         ]
+    )
+
+# Create play object, playbook objects use .load instead of init or new methods,
+# this will also automatically create the task objects from the info provided in play_source
+play = Play().load(play_source, variable_manager=variable_manager, loader=loader)
+
+# Run it - instantiate task queue manager, which takes care of forking and setting up all objects to iterate over host list and tasks
+tqm = None
+try:
+    tqm = TaskQueueManager(
+              inventory=inventory,
+              variable_manager=variable_manager,
+              loader=loader,
+              options=options,
+              passwords=passwords,
+          )
+    result = tqm.run(play) # most interesting data for a play is actually sent to the callback's methods
+finally:
+    # we always need to cleanup child procs and the structres we use to communicate with them
+    if tqm is not None:
+        tqm.cleanup()
+
+    # Remove ansible tmpdir
+    shutil.rmtree(C.DEFAULT_LOCAL_TMP, True)
