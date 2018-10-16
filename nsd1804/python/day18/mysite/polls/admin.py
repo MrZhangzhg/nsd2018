1e867faba7e5b8cd59e4ce100762b55a785443fb
MrZhangzhg <zhangzg@tedu.cn>
2018-10-16 17:08:01 +0800

    1804 py day18 17pm

--- nsd1804/python/day18/mysite/polls/admin.py
+++ nsd1804/python/day18/mysite/polls/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Question, Choice
 
-# Register your models here.
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ('question_text', 'publish_date')
+    list_filter = ('publish_date',)
+    date_hierarchy = 'publish_date'
+
+class ChoiceAdmin(admin.ModelAdmin):
+    list_display = ('question', 'choice_text', 'votes')
+
+# admin.site.register(Question)
+# admin.site.register(Choice)
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice, ChoiceAdmin)
--- nsd1804/python/day18/mysite/polls/models.py
+++ nsd1804/python/day18/mysite/polls/models.py
@@ -4,6 +4,9 @@ class Question(models.Model):
     question_text = models.CharField(max_length=200)
     publish_date = models.DateTimeField('date published')
 
+    def __str__(self):
+        return self.question_text
+
 class Choice(models.Model):
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
