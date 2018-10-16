215a6ad73c638e5472b2065ae3ffae4047d7b7b9
MrZhangzhg <zhangzg@tedu.cn>
2018-10-16 17:47:31 +0800

    1804 py day18 18pm

--- nsd1804/python/day18/mysite/polls/admin.py
+++ nsd1804/python/day18/mysite/polls/admin.py
@@ -5,9 +5,12 @@ class QuestionAdmin(admin.ModelAdmin):
     list_display = ('question_text', 'publish_date')
     list_filter = ('publish_date',)
     date_hierarchy = 'publish_date'
+    search_fields = ('question_text',)
+    ordering = ('-publish_date', 'question_text')
 
 class ChoiceAdmin(admin.ModelAdmin):
     list_display = ('question', 'choice_text', 'votes')
+    raw_id_fields = ('question',)
 
 # admin.site.register(Question)
 # admin.site.register(Choice)
