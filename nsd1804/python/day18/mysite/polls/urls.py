4c9d857635672f5dc1cdb4a5ab00647be6a25cfd
MrZhangzhg <zhangzg@tedu.cn>
2018-10-16 14:23:13 +0800

    1804 py day18 14:30pm

--- nsd1804/python/day18/mysite/polls/urls.py
+++ nsd1804/python/day18/mysite/polls/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import url
+from . import views  # django默认把项目基础目录设置为mysite(manage.py所在目录)
 
 urlpatterns = [
-
+    # 使用index函数响应polls主页请求，该url的名字(name)是index
+    url(r'^$', views.index, name='index')  # http://127.0.0.1/polls/
 ]
--- nsd1804/python/day18/mysite/polls/views.py
+++ nsd1804/python/day18/mysite/polls/views.py
@@ -1,3 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
-# Create your views here.
+# request必须提供，表示用户的请求
+def index(request):
+    return HttpResponse('<h1>Polls OK</h1>')
