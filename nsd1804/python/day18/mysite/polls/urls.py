fb4cbae7a798d69cefbaf2c8e23292d677de123b
MrZhangzhg <zhangzg@tedu.cn>
2018-10-17 14:44:50 +0800

    1804 py day19 15pm

--- nsd1804/python/day18/mysite/polls/urls.py
+++ nsd1804/python/day18/mysite/polls/urls.py
@@ -3,5 +3,6 @@ from . import views  # django默认把项目基础目录设置为mysite(manage.p
 
 urlpatterns = [
     # 使用index函数响应polls主页请求，该url的名字(name)是index
-    url(r'^$', views.index, name='index')  # http://127.0.0.1/polls/
+    url(r'^$', views.index, name='index'),  # http://127.0.0.1/polls/
+    url(r'(?P<question_id>\d+)/$', views.detail, name='detail'),
 ]
--- nsd1804/python/day18/mysite/polls/views.py
+++ nsd1804/python/day18/mysite/polls/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, get_object_or_404
 from .models import Question
 
 # request必须提供，表示用户的请求
@@ -9,3 +9,7 @@ def index(request):
     questions = Question.objects.all()
     return render(request, 'polls/index.html', {'questions': questions})
 
+def detail(request, question_id):
+    # question = Question.objects.get(id=question_id)
+    question = get_object_or_404(Question, id=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
