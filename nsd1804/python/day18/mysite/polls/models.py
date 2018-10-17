a23e07c6bf7a8a64d8fb686ec9f28fed1feb997b
MrZhangzhg <zhangzg@tedu.cn>
2018-10-17 10:03:05 +0800

    1804 py day19 10am

--- nsd1804/python/day18/mysite/polls/models.py
+++ nsd1804/python/day18/mysite/polls/models.py
@@ -11,3 +11,6 @@ class Choice(models.Model):
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.choice_text
