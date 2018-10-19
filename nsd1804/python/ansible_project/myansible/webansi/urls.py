7273a17d006a5f35f2bf44d616b0ee8b8f2392c5
MrZhangzhg <zhangzg@tedu.cn>
2018-10-19 10:35:34 +0800

    1804 py day21 10:40am

--- nsd1804/python/ansible_project/myansible/webansi/urls.py
+++ nsd1804/python/ansible_project/myansible/webansi/urls.py
@@ -1,5 +1,9 @@
 from django.conf.urls import url
+from webansi import views
 
 urlpatterns = [
-
+    url(r'^$', views.mainpage, name='mainpage'),
+    url(r'^addhosts/$', views.addhosts, name='addhosts'),
+    url(r'^addmodules/$', views.addmodules, name='addmodules'),
+    url(r'^tasks/$', views.tasks, name='tasks'),
 ]
--- nsd1804/python/ansible_project/myansible/webansi/views.py
+++ nsd1804/python/ansible_project/myansible/webansi/views.py
@@ -1,4 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
 def index(request):
     return render(request, 'index.html')
+
+def mainpage(request):
+    return HttpResponse('<h1>main page</h1>')
+
+def addhosts(request):
+    return HttpResponse('<h1>add hosts</h1>')
+
+def addmodules(request):
+    return HttpResponse('<h1>add modules</h1>')
+
+def tasks(request):
+    return HttpResponse('<h1>tasks</h1>')
