e84a72b3be9d748b65ddd7fb917bbbfa90025fda
MrZhangzhg <zhangzg@tedu.cn>
2018-10-20 15:46:42 +0800

    1804 py day22 16pm

--- nsd1804/python/ansible_project/myansible/webansi/views.py
+++ nsd1804/python/ansible_project/myansible/webansi/views.py
@@ -1,5 +1,55 @@
+import shutil
+from collections import namedtuple
+from ansible.parsing.dataloader import DataLoader
+from ansible.vars.manager import VariableManager
+from ansible.inventory.manager import InventoryManager
+from ansible.playbook.play import Play
+from ansible.executor.task_queue_manager import TaskQueueManager
+import ansible.constants as C
 from django.shortcuts import render, redirect
 from .models import HostGroup, AnsibleModule, ModuleArg, Host
+
+def exec_task(servers, mod, args):
+    Options = namedtuple('Options',
+                         ['connection', 'module_path', 'forks', 'become', 'become_method', 'become_user', 'check',
+                          'diff'])
+    options = Options(connection='smart', module_path=['/to/mymodules'], forks=10, become=None, become_method=None,
+                      become_user=None, check=False, diff=False)
+
+    loader = DataLoader()
+    passwords = dict()
+
+    inventory = InventoryManager(loader=loader, sources=['ansicfg/dhosts.py'])
+
+    variable_manager = VariableManager(loader=loader, inventory=inventory)
+
+    play_source = dict(
+        name="Ansible Play",
+        hosts=servers,
+        gather_facts='no',
+        tasks=[
+            dict(action=dict(module=mod, args=args), register='shell_out'),
+        ]
+    )
+
+    play = Play().load(play_source, variable_manager=variable_manager, loader=loader)
+
+    tqm = None
+    try:
+        tqm = TaskQueueManager(
+            inventory=inventory,
+            variable_manager=variable_manager,
+            loader=loader,
+            options=options,
+            passwords=passwords,
+        )
+        result = tqm.run(play)
+    finally:
+        if tqm is not None:
+            tqm.cleanup()
+
+        shutil.rmtree(C.DEFAULT_LOCAL_TMP, True)
+
 
 def index(request):
     return render(request, 'index.html')
@@ -29,6 +79,14 @@ def addmodules(request):
     return render(request, 'addmodules.html', {'mods': mods})
 
 def tasks(request):
+    if request.method == 'POST':
+        server = request.POST.get('ip')
+        if not server:
+            server = request.POST.get('group')
+        mod = request.POST.get('mod')
+        args = request.POST.get('arg')
+        exec_task(server, mod, args)
+
     hosts = Host.objects.all()
     groups = HostGroup.objects.all()
     mods = AnsibleModule.objects.all()
