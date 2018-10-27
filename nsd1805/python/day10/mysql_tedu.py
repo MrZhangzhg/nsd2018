c1a5e85e0a8ffb35294d95dcee842436ca4a9db8
MrZhangzhg <zhangzg@tedu.cn>
2018-10-27 10:59:09 +0800

    1805 py day10 11am

---
+++ nsd1805/python/day10/mysql_tedu.py
@@ -0,0 +1,13 @@
+import pymysql
+
+conn = pymysql.connect(
+    host='127.0.0.1',
+    port=3306,
+    user='root',
+    passwd='tedu.cn',
+    db='tedu1805',
+    charset='utf8'
+)
+cursor = conn.cursor()
+
+
