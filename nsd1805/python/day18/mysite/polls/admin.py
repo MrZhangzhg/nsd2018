5a52c6f198fcdfc3cbbd1b284a134062a2f0c086
MrZhangzhg <zhangzg@tedu.cn>
2018-11-07 17:49:14 +0800

    1805 py day18 18pm

--- nsd1805/python/day18/mysite/polls/admin.py
+++ nsd1805/python/day18/mysite/polls/admin.py
@@ -1,5 +1,13 @@
 from django.contrib import admin
 from .models import Question, Choice
 
-admin.site.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ('question_text', 'pub_date')
+    list_filter = ('pub_date',)
+    search_fields = ('question_text',)
+    date_hierarchy = 'pub_date'
+    ordering = ['-pub_date']
+
+admin.site.register(Question, QuestionAdmin)
+# admin.site.register(Question)
 admin.site.register(Choice)
