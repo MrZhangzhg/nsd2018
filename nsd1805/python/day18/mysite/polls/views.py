067c2faa81d36147a9bc91c166eb57d0f600ea55
MrZhangzhg <zhangzg@tedu.cn>
2018-11-08 17:10:53 +0800

    1805 py day19 17m

--- nsd1805/python/day18/mysite/polls/views.py
+++ nsd1805/python/day18/mysite/polls/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, redirect
 from .models import Question
 
 def index(request):
@@ -8,3 +8,19 @@ def index(request):
 def detail(request, question_id):
     question = Question.objects.get(id=question_id)
     return render(request, 'detail.html', {'question': question})
+
+def vote(request, question_id):
+    choice_id = request.POST.get('choice')
+    question = Question.objects.get(id=question_id)
+    choice = question.choice_set.get(id=choice_id)
+    choice.votes += 1
+    choice.save()
+
+    return redirect('result', question_id=question_id)
+
+def result(request, question_id):
+    question = Question.objects.get(id=question_id)
+    return render(request, 'result.html', {'question': question})
+
+
+
