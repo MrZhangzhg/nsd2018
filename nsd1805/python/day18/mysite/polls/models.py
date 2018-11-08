05cf66cecf44275076a8f9cef62be2d093ff51a0
MrZhangzhg <zhangzg@tedu.cn>
2018-11-08 11:56:58 +0800

    1805 py day19 12am

--- nsd1805/python/day18/mysite/polls/models.py
+++ nsd1805/python/day18/mysite/polls/models.py
@@ -1,8 +1,13 @@
 from django.db import models
+from datetime import timedelta
+from django.utils import timezone
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField()
+
+    def was_publish_recently(self, days=7):
+        return self.pub_date > timezone.now() - timedelta(days=days)
 
     def __str__(self):
         return self.question_text
