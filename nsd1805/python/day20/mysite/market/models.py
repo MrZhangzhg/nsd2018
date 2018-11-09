afa619e7620ab876a9f5d29cc8bef98482682902
MrZhangzhg <zhangzg@tedu.cn>
2018-11-09 12:02:36 +0800

    1805 py day20 12am

--- nsd1805/python/day20/mysite/market/models.py
+++ nsd1805/python/day20/mysite/market/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Userdb(models.Model):
+    username = models.CharField(max_length=20)
+    salt = models.CharField(max_length=8)
+    password = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.username
--- nsd1805/python/day20/mysite/market/views.py
+++ nsd1805/python/day20/mysite/market/views.py
@@ -1,5 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Userdb
+import hmac
 
 def home(request):
     return render(request, 'home.html')
 
+def login(request):
+    username = request.POST.get('username')
+    password = request.POST.get('password')
+    user = Userdb.objects.get(username=username)
+    if not user:
+        return redirect('home')
+
+    h = hmac.new(user.salt.encode(), password.encode(), digestmod='MD5')
+    if user.password != h.hexdigest():
+        return redirect('home')
+
+    request.session['LOGINED'] = True
+    return redirect('protected')
+
+
+def protected(request):
+
+    return render(request, 'protected.html')
