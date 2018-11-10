2fd2e41767dab60445ee1a8b743c2d0ec59d24d6
MrZhangzhg <zhangzg@tedu.cn>
2018-11-10 11:35:51 +0800

    1805 py day21 11:40am

--- nsd1805/python/ansible_project/myansible/webansi/models.py
+++ nsd1805/python/ansible_project/myansible/webansi/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+class HostGroup(models.Model):
+    hostgroup = models.CharField(max_length=20, unique=True)
+
+    def __str__(self):
+        return self.hostgroup
+
+class Host(models.Model):
+    hostname = models.CharField(max_length=50, unique=True)
+    ipaddr = models.CharField(max_length=15, unique=True)
+    group = models.ForeignKey(HostGroup, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return "<%s -> %s>" % (self.hostname, self.group)
+
+class AnsiModule(models.Model):
+    mod_name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.mod_name
+
+class ModArgs(models.Model):
+    args_text = models.CharField(max_length=100)
+    mod = models.ForeignKey(AnsiModule, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return "<%s -> %s>" % (self.args_text, self.mod)
