fc86463e6d7a2de2108ab120fc639035be0bc984
MrZhangzhg <zhangzg@tedu.cn>
2018-11-10 17:50:26 +0800

    1805 py day21 18pm

--- nsd1805/python/ansible_project/myansible/webansi/views.py
+++ nsd1805/python/ansible_project/myansible/webansi/views.py
@@ -8,5 +8,13 @@ def mainpage(request):
     return render(request, 'mainpage.html')
 
 def add_hosts(request):
+    if request.method == 'POST':
+        group = request.POST.get('group').strip()
+        host = request.POST.get('host').strip()
+        ipaddr = request.POST.get('ipaddr').strip()
+        if group:
+            g = HostGroup.objects.get_or_create(hostgroup=group)[0]
+            if host and ipaddr:
+                g.host_set.get_or_create(hostname=host, ipaddr=ipaddr)
     hostgroup = HostGroup.objects.all()
     return render(request, 'addhosts.html', {'hostgroup': hostgroup})
