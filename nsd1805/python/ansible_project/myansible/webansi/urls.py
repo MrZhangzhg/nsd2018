644212bae109aa58855c7aba85b8370e245aada9
MrZhangzhg <zhangzg@tedu.cn>
2018-11-10 16:37:30 +0800

    1805 py day21 16:40pm

--- nsd1805/python/ansible_project/myansible/webansi/urls.py
+++ nsd1805/python/ansible_project/myansible/webansi/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
+from . import views
 
 urlpatterns = [
-
+    url(r'^$', views.mainpage, name='mainpage'),
 ]
 
--- nsd1805/python/ansible_project/myansible/webansi/views.py
+++ nsd1805/python/ansible_project/myansible/webansi/views.py
@@ -3,3 +3,5 @@ from django.shortcuts import render
 def index(request):
     return render(request, 'index.html')
 
+def mainpage(request):
+    return render(request, 'mainpage.html')
