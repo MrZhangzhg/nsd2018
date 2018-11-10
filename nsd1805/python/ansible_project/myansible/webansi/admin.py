7caa27f40953c3237227353fdc50a4093c975624
MrZhangzhg <zhangzg@tedu.cn>
2018-11-10 14:21:51 +0800

    1805 py day21 14:30pm

--- nsd1805/python/ansible_project/myansible/webansi/admin.py
+++ nsd1805/python/ansible_project/myansible/webansi/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import HostGroup, Host, AnsiModule, ModArgs
 
-# Register your models here.
+class HostAdmin(admin.ModelAdmin):
+    list_display = ('hostname', 'ipaddr', 'group')
+    search_fields = ('hostname', 'ipaddr')
+    # 用在有外键的项目上，创建主机时，可以显示组的详细信息
+    raw_id_fields = ('group',)
+
+class HostGroupAdmin(admin.ModelAdmin):
+    search_fields = ('hostgroup', )
+
+admin.site.register(Host, HostAdmin)
+admin.site.register(HostGroup, HostGroupAdmin)
+
+for item in [AnsiModule, ModArgs]:
+    admin.site.register(item)
+
