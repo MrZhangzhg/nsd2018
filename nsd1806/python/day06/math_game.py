e2e186cb76f9365da12ec195139121026a1033fe
MrZhangzhg <zhangzg@tedu.cn>
2018-11-14 14:47:33 +0800

    1806 py day06 15pm

--- nsd1806/python/day06/math_game.py
+++ nsd1806/python/day06/math_game.py
@@ -1,11 +1,27 @@
+from random import randint, choice
+
 def exam():
     '出题，要求用户作答，答错三次，给出正确答案'
+    nums = [randint(1, 100) for i in range(2)]
+    nums.sort(reverse=True)   # 降序排列
+    op = choice('+-')
+    if op == '+':
+        result = nums[0] + nums[1]
+    else:
+        result = nums[0] - nums[1]
+    prompt = '%s %s %s = ' % (nums[0], op, nums[1])
+    answer = int(input(prompt))
+    if answer == result:
+        print('Very Good!')
+    else:
+        print('Wrong Answer!')
+
 
 def main():
     while True:
         exam()
         yn = input('Continue(y/n)? ').strip()[0]
-        if yn in ['n', 'N']:
+        if yn in 'nN':
             break
 
 if __name__ == '__main__':
---
+++ nsd1806/python/day06/math_game2.py
@@ -0,0 +1,49 @@
+from random import randint, choice
+
+def add(x, y):
+    return x + y
+
+def sub(x, y):
+    return x - y
+
+def exam():
+    '出题，要求用户作答，答错三次，给出正确答案'
+    cmds = {'+': add, '-': sub}
+    nums = [randint(1, 100) for i in range(2)]
+    nums.sort(reverse=True)   # 降序排列
+    op = choice('+-')
+    result = cmds[op](*nums)
+    prompt = '%s %s %s = ' % (nums[0], op, nums[1])
+    counter = 0
+
+    while counter < 3:
+        try:
+            answer = int(input(prompt))
+        except:
+            continue
+
+        if answer == result:
+            print('Very Good!')
+            break
+        print('Wrong Answer!')
+        counter += 1
+    else:
+        print('%s%s' % (prompt, result))
+
+
+def main():
+    while True:
+        exam()
+        try:
+            yn = input('Continue(y/n)? ').strip()[0]
+        except IndexError:
+            continue
+        except (KeyboardInterrupt, EOFError):
+            print('\nBye-bye')
+            yn = 'n'
+
+        if yn in 'nN':
+            break
+
+if __name__ == '__main__':
+    main()
