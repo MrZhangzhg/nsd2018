2ef8a8c1d4ab24e61dd2b2e88fe14d2a3c46746e
MrZhangzhg <zhangzg@tedu.cn>
2018-11-15 15:20:46 +0800

    1806 py day07 15:30pm

--- nsd1806/python/day07/backup.py
+++ nsd1806/python/day07/backup.py
@@ -36,14 +36,34 @@ def full_backup(src_dir, dst_dir, md5_file):
 
 
 def incr_backup(src_dir, dst_dir, md5_file):
-    pass
+    fname = os.path.basename(src_dir.rstrip('/'))
+    fname = '%s_incr_%s.tar.gz' % (fname, time.strftime('%Y%m%d'))
+    fname = os.path.join(dst_dir, fname)
+    md5dict = {}
+
+    for path, folders, files in os.walk(src_dir):
+        for file in files:
+            key = os.path.join(path, file)
+            md5dict[key] = check_md5(key)
+
+    with open(md5_file, 'rb') as fobj:
+        old_md5 = p.load(fobj)
+
+    with open(md5_file, 'wb') as fobj:
+        p.dump(md5dict, fobj)
+
+    tar = tarfile.open(fname, 'w:gz')
+    for key in md5dict:
+        if old_md5.get(key) != md5dict[key]:
+            tar.add(key)
+    tar.close()
 
 
 if __name__ == '__main__':
     src_dir = '/tmp/demo/security/'
     dst_dir = '/tmp/demo/backup/'
     md5_file = '/tmp/demo/backup/md5.data'
-    if time.strftime('%a') == 'Thu':
+    if time.strftime('%a') == 'Mon':
         full_backup(src_dir, dst_dir, md5_file)
     else:
         incr_backup(src_dir, dst_dir, md5_file)
