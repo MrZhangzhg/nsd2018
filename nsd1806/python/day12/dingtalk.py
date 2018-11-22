0a0d0dac5985f7d0de407b1ccbcb06a35c6fdf5c
MrZhangzhg <zhangzg@tedu.cn>
2018-11-22 15:41:05 +0800

    1806 py day12 16pm

---
+++ nsd1806/python/day12/dingtalk.py
@@ -0,0 +1,40 @@
+import json
+import requests
+import sys
+
+def send_msg(url, reminders, msg):
+    headers = {'Content-Type': 'application/json;charset=utf-8'}
+    data = {
+        "msgtype": "text",  # 发送消息类型为文本
+        "at": {
+            "atMobiles": reminders,
+            "isAtAll": False,   # 不@所有人
+        },
+        "text": {
+            "content": msg,   # 消息正文
+        }
+    }
+    r = requests.post(url, data=json.dumps(data), headers=headers)
+    return r.text
+
+def send_msg2(url, reminders, msg):
+    headers = {'Content-Type': 'application/json;charset=utf-8'}
+    data = {
+        "msgtype": "markdown",  # 发送消息类型为文本
+        "at": {
+            "atMobiles": reminders,
+            "isAtAll": False,   # 不@所有人
+        },
+        "markdown": {
+            "title": "杭州天气",
+            "text":"#### 杭州天气  \n > 9度，@1825718XXXX 西北风1级，空气良89，相对温度73%\n\n > ![screenshot](http://i01.lw.aliimg.com/media/lALPBbCc1ZhJGIvNAkzNBLA_1200_588.png)\n  > ###### 10点20分发布 [天气](http://www.thinkpage.cn/) "
+        }
+    }
+    r = requests.post(url, data=json.dumps(data), headers=headers)
+    return r.text
+
+if __name__ == '__main__':
+    msg = sys.argv[1]
+    reminders = ['15055667788']  # 特殊提醒要查看的人,就是@某人一下
+    url = '机器人webhook地址'
+    print(send_msg2(url, reminders, msg))
