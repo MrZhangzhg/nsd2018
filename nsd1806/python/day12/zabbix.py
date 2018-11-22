b6fd0d41d2f716b62f8e7b98a6f11caffae70615
MrZhangzhg <zhangzg@tedu.cn>
2018-11-22 18:04:09 +0800

    1806 py day12 18pm

--- nsd1806/python/day12/zabbix.py
+++ nsd1806/python/day12/zabbix.py
@@ -22,11 +22,93 @@ header = {'Content-Type': 'application/json-rpc'}
 # }
 # 540dcb9510a41ef32a78b4071d99dc76
 ################################
+# 获取所有主机的信息
+# data = {
+#     "jsonrpc": "2.0",
+#     "method": "host.get",
+#     "params": {
+#         "output": "extend",
+#     },
+#     "auth": "540dcb9510a41ef32a78b4071d99dc76",
+#     "id": 1
+# }
+################################
+# 将主机ID是10257和10258的主机从zabbix中删除
+# data = {
+#     "jsonrpc": "2.0",
+#     "method": "host.delete",
+#     "params": [
+#         "10257",   # 被删除的主机id
+#         "10258"
+#     ],
+#     "auth": "540dcb9510a41ef32a78b4071d99dc76",
+#     "id": 1
+# }
+################################
+# 获取Linux Servers 组的信息，如groupid
+# data = {
+#     "jsonrpc": "2.0",
+#     "method": "hostgroup.get",
+#     "params": {
+#         "output": "extend",
+#         "filter": {
+#             "name": [
+#                 "Linux servers"
+#             ]
+#         }
+#     },
+#     "auth": "540dcb9510a41ef32a78b4071d99dc76",
+#     "id": 1
+# }
+#
+################################
+# 获取Template OS Linux模板信息，如它的ID
+# data = {
+#     "jsonrpc": "2.0",
+#     "method": "template.get",
+#     "params": {
+#         "output": "extend",
+#         "filter": {
+#             "host": [
+#                 "Template OS Linux",
+#             ]
+#         }
+#     },
+#     "auth": "540dcb9510a41ef32a78b4071d99dc76",
+#     "id": 1
+# }
+################################
+
 data = {
     "jsonrpc": "2.0",
-    "method": "host.get",
+    "method": "host.create",
     "params": {
-        "output": "extend",
+        "host": "web_server1",
+        "interfaces": [
+            {
+                "type": 1,
+                "main": 1,
+                "useip": 1,
+                "ip": "192.168.4.10",
+                "dns": "",
+                "port": "10050"
+            }
+        ],
+        "groups": [
+            {
+                "groupid": "2"
+            }
+        ],
+        "templates": [
+            {
+                "templateid": "10001"
+            }
+        ],
+        "inventory_mode": 0,
+        "inventory": {
+            "macaddress_a": "01234",
+            "macaddress_b": "56768"
+        }
     },
     "auth": "540dcb9510a41ef32a78b4071d99dc76",
     "id": 1
