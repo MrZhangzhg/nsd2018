88a1f058fedbba387e91212c543d543b5861fa41
MrZhangzhg <zhangzg@tedu.cn>
2018-12-03 10:50:01 +0800

    1806 py day20 11am

--- nsd1806/python/day20/blog/article/views.py
+++ nsd1806/python/day20/blog/article/views.py
@@ -1,3 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
-# Create your views here.
+def hello(request):
+    return HttpResponse('<h1>Hello World!</h1>')
