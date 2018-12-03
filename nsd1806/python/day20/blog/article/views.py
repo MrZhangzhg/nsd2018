c05f060065c338a74c954f1bedad6bdd3bc1707b
MrZhangzhg <zhangzg@tedu.cn>
2018-12-03 11:37:54 +0800

    1806 py day20 11:40am

--- nsd1806/python/day20/blog/article/views.py
+++ nsd1806/python/day20/blog/article/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render, HttpResponse
+from .models import Article
+
+def index(request):
+    articles = Article.objects.order_by('-pub_date')
+    return render(request, 'index.html', {'articles': articles})
 
 def hello(request):
     return HttpResponse('<h1>Hello World!</h1>')
