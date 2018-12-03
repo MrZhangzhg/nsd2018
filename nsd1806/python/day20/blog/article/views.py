6ea535f5c5ac5604c651859c73f3936a0e7bbd2f
MrZhangzhg <zhangzg@tedu.cn>
2018-12-03 14:19:44 +0800

    1806 py day20 14pm

--- nsd1806/python/day20/blog/article/views.py
+++ nsd1806/python/day20/blog/article/views.py
@@ -1,7 +1,19 @@
 from django.shortcuts import render, HttpResponse
 from .models import Article
+from django.utils import timezone
 
 def index(request):
+    if request.method == 'POST':
+        # print(request.POST)
+        title = request.POST.get('title')
+        content = request.POST.get('content')
+        if title.strip() and content.strip():
+            Article.objects.create(
+                title=title,
+                pub_date= timezone.now(),
+                text=content
+            )
+
     articles = Article.objects.order_by('-pub_date')
     return render(request, 'index.html', {'articles': articles})
 
