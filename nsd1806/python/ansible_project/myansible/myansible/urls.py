d67b61cd66007ac34ac206db74a59bafc5e22b21
MrZhangzhg <zhangzg@tedu.cn>
2018-12-04 11:42:46 +0800

    1806 py day21 11:40am

--- nsd1806/python/ansible_project/myansible/myansible/urls.py
+++ nsd1806/python/ansible_project/myansible/myansible/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from . import views
 
 urlpatterns = [
+    url(r'^$', views.index, name='index'),
     url(r'^admin/', admin.site.urls),
     url(r'^webansi/', include('webansi.urls')),
 ]
