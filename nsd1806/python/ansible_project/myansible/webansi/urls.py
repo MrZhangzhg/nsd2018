2dd1e33038bf9a87562d7cc494944cf20d311a12
MrZhangzhg <zhangzg@tedu.cn>
2018-12-05 09:34:15 +0800

    1806 py day22 9:30am

--- nsd1806/python/ansible_project/myansible/webansi/urls.py
+++ nsd1806/python/ansible_project/myansible/webansi/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.home, name='polls_index'),
+    url(r'^addhosts/$', views.addhosts, name='addhosts'),
 ]
--- nsd1806/python/ansible_project/myansible/webansi/views.py
+++ nsd1806/python/ansible_project/myansible/webansi/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'home.html')
+
+def addhosts(request):
+    return render(request, 'addhosts.html')
